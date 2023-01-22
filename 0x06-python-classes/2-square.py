b843608b71be39ac634d392d56c965fb94bced09
Zakiya Abass <zakiyaabass@icloud.com>
2023-01-22 22:49:54 +0300

    3-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"""Defines a class 'Square'"""
 
 
 class Square:
-    """Represent a square."""
+    """Represents a square"""
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Initializes a new Square.
         Args:
-            size (int): The size of the new square.
+            size (int): size of the new square.
         """
-        if not isinstance(size, int):
+        if isinstance(size, int):
+            self.__size = size
+        else:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a class 'Square'"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+        Args:
+            size (int): size of new square
+        """
+
+        self.size = size
+
+    def area(self):
+        """Returns the area of instance of square"""
+        return (pow(self.__size, 2))
+
+    @property
+    def size(self):
+        """Gets the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the current size of the square."""
+        if isinstance(value, int):
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
